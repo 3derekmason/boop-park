fdba33b111d8bf3acb7445aacec218c3f1658868
Derek Mason <3derekmason@gmail.com>
2022-05-17 18:30:04 -0600

    bcrypt for new user 9/10

--- pages/api/controllers/users.js
+++
@@ -1,1 +0,0 @@
-// mongodb+srv://derekmason:boop@boop.y55kq.mongodb.net/?retryWrites=true&w=majority
--- pages/api/users.js
+++ pages/api/users.js
@@ -1,3 +1,28 @@
-export default function handler(req, res) {
-  res.status(200).json("All Users");
+const mongoose = require("mongoose");
+
+const User = require("./db/models/user.js");
+
+export default async function handler(req, res) {
+  const connection =
+    "mongodb+srv://derekmason:boop@boop.y55kq.mongodb.net/?retryWrites=true&w=majority";
+  mongoose.connect(connection, function (err) {
+    if (err) throw err;
+    console.log("Successfully connected to MongoDB");
+  });
+  if (req.method === "POST") {
+    console.log("body", req.body);
+    const newUser = new User({
+      username: req.body.username,
+      password: req.body.password,
+      first_name: req.body.first,
+      last_name: req.body.last,
+      joined: new Date(),
+    });
+    newUser.save(function (err) {
+      if (err) throw err;
+    });
+    res.status(201).send(newUser);
+  } else {
+    res.status(200).json("All Users");
+  }
 }
