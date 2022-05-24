19957d68c00fe10453d0501852d4eb59091f6138
Derek Mason <3derekmason@gmail.com>
2022-05-24 09:34:09 -0600

    redirects init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  async redirects() {
+    return [{ source: "/login", destination: "/login", permanent: true }];
+  },
+  nextConfig: {
+    reactStrictMode: true,
+  },
+};
