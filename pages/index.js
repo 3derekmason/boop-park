908bb1c8279cbcbcbdaba08a7f75cdd5c388a19f
Derek Mason <3derekmason@gmail.com>
2022-05-24 11:36:58 -0600

    redirect to login page if no user

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+
 const HomePage = () => {
-  return <div>Welcome to Next.js!</div>;
+  const router = useRouter();
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    function toLogin() {
+      router.push("/login");
+    }
+    if (!currentUser) {
+      toLogin();
+    }
+  }, [currentUser]);
+
+  return <div className="homepage">Home Page</div>;
 };
 
 export default HomePage;
