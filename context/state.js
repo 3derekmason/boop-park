aa4eee45aac1e34496135ed450c2d7e8daf89d99
Derek Mason <3derekmason@gmail.com>
2022-05-24 15:03:25 -0600

    add shared context

---
+++ context/state.js
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+
+const AppContext = createContext();
+
+export function AppWrapper() {
+  const [currentUser, setCurrentUser] = useState();
+
+  let sharedState = {
+    currentUser,
+  };
+
+  return (
+    <AppContext.Provider value={sharedState}>{children}</AppContext.Provider>
+  );
+}
+
+export function useAppContext() {
+  return useContext(AppContext);
+}
