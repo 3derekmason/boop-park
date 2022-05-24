008affb89b7459a3248e3e618ee01afb6ed6805a
Derek Mason <3derekmason@gmail.com>
2022-05-24 14:30:17 -0600

    login page css module

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,4 +1,15 @@
+import { Paper, Typography } from "@mui/material";
+
+import styles from "./login.module.css";
 const LoginPage = () => {
-  return <div className="loginPage">This is the log in page...</div>;
+  return (
+    <div className={styles.loginPage}>
+      <Paper elevation={4} className={styles.loginSideBar}>
+        <Typography component={"p"} variant="caption">
+          Not a user? Sign in here.
+        </Typography>
+      </Paper>
+    </div>
+  );
 };
 export default LoginPage;
