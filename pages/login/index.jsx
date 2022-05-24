d745f39ac802110483f1b235a743c66d694c8ae6
Derek Mason <3derekmason@gmail.com>
2022-05-24 14:44:52 -0600

    login page template

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,14 +1,78 @@
-import { Paper, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 import styles from "./login.module.css";
 const LoginPage = () => {
+  const handleSubmit = () => {
+    console.log("Submit :)");
+  };
+
   return (
     <div className={styles.loginPage}>
-      <Paper elevation={4} className={styles.loginSideBar}>
-        <Typography component={"p"} variant="caption">
-          Not a user? Sign in here.
-        </Typography>
-      </Paper>
+      <Container component="main" maxWidth="xs">
+        <Paper elevation={4} className={styles.loginSideBar}>
+          <Box className={styles.loginForm}>
+            <Avatar sx={{ m: 1 }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+            </Box>
+            <Typography component={"p"} variant="caption">
+              Not a user? Sign in here.
+            </Typography>
+          </Box>
+        </Paper>
+      </Container>
     </div>
   );
 };
