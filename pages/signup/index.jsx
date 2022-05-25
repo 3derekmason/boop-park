869c9884ac314d05b94d3ee5f71bc8d5cd94046b
Derek Mason <3derekmason@gmail.com>
2022-05-25 13:11:17 -0600

    refactor sign up page

--- pages/signup/index.jsx
+++ pages/signup/index.jsx
@@ -11,14 +11,20 @@ import {
   Container,
 } from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { useState } from "react";
 
 const SignUpPage = () => {
+  const [errMessage, toggleErrMessage] = useState(false);
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
+    if (data.get("password") !== data.get("verifyPassword")) {
+      return toggleErrMessage(true);
+    }
     console.log({
-      email: data.get("email"),
+      username: data.get("username"),
       password: data.get("password"),
+      verify: data.get("verifyPassword"),
     });
   };
 
@@ -32,7 +38,7 @@ const SignUpPage = () => {
           alignItems: "center",
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+        <Avatar>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
@@ -40,35 +46,14 @@ const SignUpPage = () => {
         </Typography>
         <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="given-name"
-                name="firstName"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="family-name"
-              />
-            </Grid>
             <Grid item xs={12}>
               <TextField
                 required
                 fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
               />
             </Grid>
             <Grid item xs={12}>
@@ -83,11 +68,16 @@ const SignUpPage = () => {
               />
             </Grid>
             <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
+              <TextField
+                required
+                fullWidth
+                name="verifyPassword"
+                label="Confirm Password:"
+                type="password"
+                id="verifyPassword"
               />
             </Grid>
+            {errMessage ? "Passwords must match" : ""}
           </Grid>
           <Button
             type="submit"
@@ -99,7 +89,7 @@ const SignUpPage = () => {
           </Button>
           <Grid container justifyContent="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
