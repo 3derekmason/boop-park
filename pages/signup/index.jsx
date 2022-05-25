d8e8d7c0642e53fbb918e2d3a0558bb1961bcaa3
Derek Mason <3derekmason@gmail.com>
2022-05-25 13:25:30 -0600

    move link to center

--- pages/signup/index.jsx
+++ pages/signup/index.jsx
@@ -93,7 +93,7 @@ const SignUpPage = () => {
             >
               Sign Up
             </Button>
-            <Grid container justifyContent="flex-end">
+            <Grid container justifyContent="center">
               <Grid item>
                 <Link href="/" variant="body2">
                   Already have an account? Sign in
