14e1792c3f0e165df9972fb836113880e355933a
Derek Mason <3derekmason@gmail.com>
2022-05-24 22:33:14 -0600

    add linear progress to loading

--- pages/components/Loading.jsx
+++ pages/components/Loading.jsx
@@ -1,5 +1,14 @@
+import LinearProgress from "@mui/material/LinearProgress";
+import Box from "@mui/material/Box";
+
 const Loading = () => {
-  return <div className="loading">Loading...</div>;
+  return (
+    <div className="loading">
+      <Box sx={{ width: "100%" }}>
+        <LinearProgress />
+      </Box>
+    </div>
+  );
 };
 
 export default Loading;
