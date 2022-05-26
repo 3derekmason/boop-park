5f0ef17bc57f67a3c580dd624cd82454b62b8e18
Derek Mason <3derekmason@gmail.com>
2022-05-26 12:37:22 -0600

    add level and change level function

--- pages/components/WorkoutModal.jsx
+++ pages/components/WorkoutModal.jsx
@@ -3,14 +3,26 @@ import {
   CardMedia,
   Button,
   Grid,
+  IconButton,
   Paper,
   Typography,
   Modal,
 } from "@mui/material";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { useState } from "react";
 import styles from "./workoutModal.module.css";
 
 const WorkoutModal = ({ workout, open, handleClose }) => {
+  const levels = [1, 2, 3];
+  const [chosenLevel, setChosenLevel] = useState(levels[0]);
+  const levelUp = () => {
+    setChosenLevel(chosenLevel + 1);
+  };
+  const levelDown = () => {
+    setChosenLevel(chosenLevel - 1);
+  };
+
   return (
     <Modal
       open={open}
@@ -43,10 +55,33 @@ const WorkoutModal = ({ workout, open, handleClose }) => {
               </Typography>
             </div>
           </div>
+          <div className={styles.levelRow}>
+            <Typography component="p" variant="body1">
+              Choose Level:
+            </Typography>
+            <IconButton
+              disabled={chosenLevel === levels[0]}
+              aria-label="previous"
+              onClick={levelDown}
+            >
+              <ArrowBackIosIcon />
+            </IconButton>
+            <Typography component="h4" variant="h5" color="primary">
+              {chosenLevel}
+            </Typography>
+            <IconButton
+              disabled={chosenLevel === levels[levels.length - 1]}
+              aria-label="previous"
+              onClick={levelUp}
+            >
+              <ArrowForwardIosIcon />
+            </IconButton>
+          </div>
           <div className={styles.timeAndSubmit}>
             <Typography component="p" variant="caption">
-              Estimated time: {workout?.estTime}
+              Estimated time: {workout?.level?.estTime || "n/a"}
             </Typography>
+
             <Button fullWidth variant="contained" color="secondary">
               Begin
             </Button>
