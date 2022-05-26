93d67ecca8971e6daf5d9087f63e1fffd760bc61
Derek Mason <3derekmason@gmail.com>
2022-05-25 22:32:56 -0600

    change main color for contrast and my own pleasure

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#00706a",
+      main: "#213937",
       light: "#67a09b",
     },
     secondary: {
