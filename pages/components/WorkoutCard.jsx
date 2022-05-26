e38b2f106ea1cc047dd8209d9925acb68f0e177b
Derek Mason <3derekmason@gmail.com>
2022-05-25 22:50:24 -0600

    make workout cards buttons

--- pages/components/WorkoutCard.jsx
+++ pages/components/WorkoutCard.jsx
@@ -11,28 +11,37 @@ import styles from "./workoutCard.module.css";
 const WorkoutCard = ({ data }) => {
   return (
     <Grid item xs={12} sm={6} md={4}>
-      <Card
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-        className={styles.workoutCard}
-      >
-        <CardMedia
-          component="img"
-          image="https://source.unsplash.com/PHIgYUGQPvU"
-          alt={`${data?.workout?.title} image`}
-        />
-        <CardContent sx={{ flexGrow: 1 }}>
-          <Typography gutterBottom variant="h5" component="h2">
-            {data?.workout?.title}
-          </Typography>
-          <Typography>{data?.workout?.desc}</Typography>
-        </CardContent>
-        {/* <CardActions>
+      <Button style={{ textTransform: "none" }}>
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+          className={styles.workoutCard}
+        >
+          <CardMedia
+            component="img"
+            image="https://source.unsplash.com/PHIgYUGQPvU"
+            alt={`${data?.workout?.title} image`}
+          />
+          <CardContent sx={{ flexGrow: 1 }}>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="h2"
+              sx={{ fontSize: 40, fontWeight: 200 }}
+            >
+              {data?.workout?.title}
+            </Typography>
+            <Typography sx={{ fontWeight: 500 }}>
+              {data?.workout?.desc}
+            </Typography>
+          </CardContent>
+          {/* <CardActions>
           <Button size="small">View</Button>
         </CardActions> */}
-      </Card>
+        </Card>
+      </Button>
     </Grid>
   );
 };
