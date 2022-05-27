2f7957cd56850a0866035b0ed3e71317ad2ec5e4
Derek Mason <3derekmason@gmail.com>
2022-05-27 16:14:50 -0600

    init syles and add appbar

--- pages/workout/index.jsx
+++ pages/workout/index.jsx
@@ -1,27 +1,37 @@
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { useAppContext } from "../../context/state";
+import styles from "./workout.module.css";
+import HomeAppBar from "../components/HomeAppbar";
 
 const WorkoutPage = () => {
   const { currentWorkout, setCurrentWorkout } = useAppContext();
   return (
-    <Container maxWidth="sm">
-      <Typography
-        component="h1"
-        variant="h2"
-        align="center"
-        color="primary"
-        gutterBottom
-      >
-        {currentWorkout?.title}
-      </Typography>
-      <Typography variant="h5" align="center" color="secondary" paragraph>
-        Level {currentWorkout?.rx?.level}
-      </Typography>
-      <Stack sx={{ pt: 4 }} direction="row" spacing={2} justifyContent="center">
-        <Button variant="contained">Back to Home</Button>
-        <Button variant="outlined">Next</Button>
-      </Stack>
-    </Container>
+    <div className={styles.workoutPage}>
+      <HomeAppBar />
+      <Box className={styles.workoutContainer}>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="primary"
+          gutterBottom
+        >
+          {currentWorkout?.title}
+        </Typography>
+        <Typography variant="h5" align="center" color="secondary" paragraph>
+          Level {currentWorkout?.rx?.level}
+        </Typography>
+        <Stack
+          sx={{ pt: 4 }}
+          direction="row"
+          spacing={2}
+          justifyContent="center"
+        >
+          <Button variant="contained">Back to Home</Button>
+          <Button variant="outlined">Next</Button>
+        </Stack>
+      </Box>
+    </div>
   );
 };
 export default WorkoutPage;
