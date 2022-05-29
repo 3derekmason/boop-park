ad215d0b0e6b179f61286cf6ebcda911298a8b92
Derek Mason <3derekmason@gmail.com>
2022-05-28 20:20:36 -0600

    center images on exercise

--- pages/workout/components/Exercise.jsx
+++ pages/workout/components/Exercise.jsx
@@ -5,14 +5,19 @@ import Image from "material-ui-image";
 const Exercise = ({ exerciseData }) => {
   return (
     <div className={styles.exercise}>
-      <Typography color="primary" component="h2" variant="h4">
+      <Typography color="primary" component="h2" variant="h5">
         {exerciseData?.name || ""}
       </Typography>
-      <div className={styles.helperImages}>
+      <div className={styles.helperPics}>
         {exerciseData?.pics?.map((picture, i) => {
-          console.log(picture);
           return (
             <Image
+              imageStyle={{
+                width: 240,
+                height: 160,
+                position: "relative",
+              }}
+              style={{ position: "relative", padding: 0 }}
               key={i}
               src={picture || ""}
               alt={`${exerciseData?.name} image`}
